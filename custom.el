7891db97a7f8b5a8a8daa50dc245c2d36f97c7ad
Federico <federicolucaspontoriero@gmail.com>
2022-09-16 14:15:11 -0300

    fixed parsing error from ts-ls

--- custom.el
+++ custom.el
@@ -41,10 +41,20 @@
     (cons 320 "#8d7374")
     (cons 340 "#37474F")
     (cons 360 "#37474F")))
- '(vc-annotate-very-old-color nil))
+ '(vc-annotate-very-old-color nil)
+ '(warning-suppress-types
+   '((lsp-mode)
+     (lsp-mode)
+     (lsp-mode)
+     (lsp-mode)
+     (lsp-mode)
+     (lsp-mode)
+     (lsp-mode)
+     (lsp-mode)
+     (lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ts-fold-replacement-face ((t (:foreground nil :box nil :inherit font-lock-comment-face :weight light)))))
